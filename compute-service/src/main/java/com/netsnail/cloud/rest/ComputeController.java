bacedf637eb8d5c7715923eea9e94849cc453af1
Tiger <tiger@netsnail.com>
2018-04-02 16:13:23 +0800

    add zuul, bus

--- compute-service/src/main/java/com/netsnail/cloud/rest/ComputeController.java
+++ compute-service/src/main/java/com/netsnail/cloud/rest/ComputeController.java
@@ -3,12 +3,15 @@ package com.netsnail.cloud.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class ComputeController {
 
@@ -17,10 +20,12 @@ public class ComputeController {
     @Autowired
     private DiscoveryClient client;
 
+    @Value("${from}") String from;
+
     @RequestMapping(value = "/add" ,method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
         Integer r = a + b;
-        logger.info("/add result:" + r);
+        logger.info("/add result ["+from+"]:" + r);
         return r;
     }
 
